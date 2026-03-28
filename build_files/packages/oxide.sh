3c569764525455095f700c0adf6da8cde551a4a9
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2026-03-28 16:31:42 -0400

    Add nvim

--- build_files/packages/oxide.sh
+++ build_files/packages/oxide.sh
@@ -2,9 +2,12 @@
 
 set -ouex pipefail
 
-dnf5 install -y sudo sudo-rs uutils-coreutils
+dnf5 install -y sudo sudo-rs uutils-coreutils neovim
 rm -rf /usr/local && mkdir -p /usr/local/bin/
 ln -sf /usr/bin/sudo-rs /usr/local/bin/sudo
+
+ln -sf /usr/bin/nvim /usr/local/bin/vim
+ln -sf /usr/bin/nvim /usr/local/bin/vi
 
 UUTILS_BIN_DIR="/usr/bin"
 for bin_path in "$UUTILS_BIN_DIR"/uu_*; do
